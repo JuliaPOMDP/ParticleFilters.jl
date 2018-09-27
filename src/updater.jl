3ac131688a23ee1ed29f5b1290e9845c9f1d0243
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-26 18:03:05 -0700

    resample refactor

--- src/updater.jl
+++ src/updater.jl
@@ -1,3 +1,9 @@
-initialize_belief(up::SimpleParticleFilter{S}, d::Any) where {S} = resample(up.resample, d, S, up.rng)
+function initialize_belief(up::SimpleParticleFilter{S}, d::Any) where {S}
+    resample(up.resample, d, up.rng)
+end
+
+function initialize_belief(up::SimpleParticleFilter{S,<:Any,R}, d::Any) where {S, R<:Union{LowVarianceResampler, ImportanceResampler}}
+    resample(up.resample, d, S, up.rng)
+end
 
 resample(f::Function, d::Any, rng::AbstractRNG) = f(d, rng)
