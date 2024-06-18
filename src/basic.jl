f0052f05f7502e94e17014e8a620141b1c64602d
jihodori <jiho8713@gmail.com>
2024-06-18 00:40:23 -0600

    fix indentation

--- src/basic.jl
+++ src/basic.jl
@@ -33,7 +33,7 @@ function BasicParticleFilter(pmodel, rmodel, resampler, n::Integer, rng::Abstrac
                                resampler,
                                n,
                                rng,
-							   resampling_threshold,
+                               resampling_threshold,
                                particle_memory(pmodel),
                                Float64[]
                               )
@@ -53,7 +53,7 @@ function update(up::BasicParticleFilter, b::AbstractParticleBelief, a, o)
     wm = up._weight_memory
     resize!(pm, n_particles(b))
     resize!(wm, n_particles(b))
-	if (calculate_ess(wm) < up.resampling_threshold)
+    if (calculate_ess(wm) < up.resampling_threshold)
         resampled_particle_collection = resample(
             up.resampler,
             WeightedParticleBelief(pm, wm, sum(wm), nothing),
@@ -63,10 +63,10 @@ function update(up::BasicParticleFilter, b::AbstractParticleBelief, a, o)
             up.rng)
 		num_particles = n_particles(resampled_particle_collection)
         return WeightedParticleBelief(resampled_particle_collection.particles, fill(1.0 / num_particles, num_particles))
-	end
+    end
     predict!(pm, up.predict_model, b, a, o, up.rng)
     reweight!(wm, up.reweight_model, b, a, pm, o, up.rng)
-	return WeightedParticleBelief(pm, wm, sum(wm), nothing)
+    return WeightedParticleBelief(pm, wm, sum(wm), nothing)
 end
 
 function Random.seed!(f::BasicParticleFilter, seed)
