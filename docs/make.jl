bf1e92a53f5405259ac4ba6512474c6071ffea65
Zachary Sunberg <zachary.sunberg@colorado.edu>
2024-07-17 14:37:12 -0600

    noted v1.0.0 hard-coding

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(
     format=Documenter.HTML(),
     sitename="ParticleFilters.jl",
     warnonly=[:missing_docs, :cross_references],
-    remotes=Dict(dirname(dirname(pathof(POMDPs))) => (Remotes.GitHub("JuliaPOMDP", "POMDPs.jl"), "v1.0.0"))
+    remotes=Dict(dirname(dirname(pathof(POMDPs))) => (Remotes.GitHub("JuliaPOMDP", "POMDPs.jl"), "v1.0.0")) # Note: this is hard-coded to version 1.0.0 because I didn't know how to fix it. It should be updated in a future release.
 )
 
 PlutoSliderServer.export_directory(
@@ -15,6 +15,4 @@ PlutoSliderServer.export_directory(
 
 deploydocs(
     repo="github.com/JuliaPOMDP/ParticleFilters.jl.git",
-    # target="build",
-    # push_preview = true,
 )
