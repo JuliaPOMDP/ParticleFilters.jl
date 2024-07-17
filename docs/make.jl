cd9d264c793ea97b489cb260bc9d84f24334d48d
Zachary Sunberg <sunbergzach@gmail.com>
2024-07-17 15:15:21 -0600

    Trying to fix documentation (#69)
    * commented out some things that seem unnecessary
    
    * tried remotes
    
    * noted v1.0.0 hard-coding
    
    * updated comment

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,8 @@ makedocs(
     modules=[ParticleFilters, POMDPs],
     format=Documenter.HTML(),
     sitename="ParticleFilters.jl",
-    warnonly=[:missing_docs, :cross_references]
+    warnonly=[:missing_docs, :cross_references],
+    remotes=Dict(dirname(dirname(pathof(POMDPs))) => (Remotes.GitHub("JuliaPOMDP", "POMDPs.jl"), "v1.0.0")) # Note: this is hard-coded to version 1.0.0 because I didn't know how to fix it. It should be updated in a future release, but it does not seem that important because it is only for source links.
 )
 
 PlutoSliderServer.export_directory(
@@ -14,6 +15,4 @@ PlutoSliderServer.export_directory(
 
 deploydocs(
     repo="github.com/JuliaPOMDP/ParticleFilters.jl.git",
-    target="build",
-    push_preview = true,
 )
