dfdb8e50c955e9e58a0a34c771665ab22f22b789
Zachary Sunberg <zachary.sunberg@colorado.edu>
2025-04-26 10:05:24 -0600

    try to force fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -23,4 +23,12 @@ makedocs(
 deploydocs(
     repo="github.com/JuliaPOMDP/ParticleFilters.jl.git",
     push_preview = true,
+    devbranch = "master",
+    # the manual versions below is a hack... hopefully some day it can get removed
+    versions = ["stable" => "v0.6.1",
+                "dev" => "master",
+                "v0.6",
+                "v0.5",
+                "v0.4",
+                "v0.3"],
 )
